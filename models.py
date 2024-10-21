14097e52d4dd29b6ec9e746923c1bc7e587fa579
Nero <engoyan.sergei@yandex.ru>
2024-10-21 16:58:57 +0300

    fetch: schems

--- models.py
+++ models.py
@@ -18,3 +18,28 @@ from sqlalchemy import (
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Session
 import pandas as pd
+
+from schema import *
+
+metadata_obj = MetaData()
+
+class BaseTabel(DeclarativeBase):
+    _schema = BaseModel
+
+    def create_table(self):
+        # Считывает xlsx файл
+        # Переименовывает столбцы и заменяет отсуттвующие данные на None
+        # Построчно валидирует данные через Pydantic и вносит их в таблицу
+        data = pd.read_excel(os.path.join("DB", self._schema.table_name))
+        data = data.rename(
+            columns=dict(zip(data.columns, list(self._schema.model_fields)))
+        )
+        data.replace({np.nan: None}, inplace=True)
+        engine = create_engine("sqlite:///DB/DataBase.sqlite", echo=False)
+        with Session(engine) as session:
+            for r in range(data.shape[0]):
+                table_model = self._schema.model_validate(data.iloc[r].to_dict())
+                stmt = insert(self.__table__).values(table_model.dict())
+                session.execute(stmt)
+            session.commit()
+            session.close()
---
+++ schema.py
@@ -0,0 +1,67 @@
+from typing import Optional, ClassVar
+
+from pydantic import BaseModel, Field, validator
+
+class Main_Table(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    idlistedu: int = Field()
+    idparent: int = Field()
+    progid: int = Field()
+    fieldid: str = Field()
+    profile: Optional[str] = Field(default=None)
+    formname: str = Field()
+    course1: Optional[int] = Field(default=None)
+    course2: Optional[int] = Field(default=None)
+    course3: Optional[int] = Field(default=None)
+    course4: Optional[int] = Field(default=None)
+    course5: Optional[int] = Field(default=None)
+    course6: Optional[int] = Field(default=None)
+    course7: Optional[int] = Field(default=None)
+    table_name: ClassVar[str] = "main.xlsx"
+
+class VUZ(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    idlistedu: int = Field()
+    idparent: int = Field()
+    fullname: str = Field()
+    name: str = Field()
+    adress: str = Field()
+    rector: str = Field()
+    id_region: int = Field()
+    id_district: int = Field()
+    id_ministry: int = Field()
+    table_name: ClassVar[str] = "Вузы.xlsx"
+
+class Programms(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    progid: int = Field()
+    progname: str = Field()
+    progcode: int = Field()
+    table_name: ClassVar[str] = "Программы.xlsx"
+
+class Trainings(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    fieldif: str = Field()
+    fieldname: str = Field()
+    progid: int = Field()
+    progcode: int = Field()
+    table_name: ClassVar[str] = "Направления.xlsx"
+
+class Regions(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    id_region: int = Field()
+    region: str = Field()
+    id_district: int = Field()
+    table_name: ClassVar[str] = "Регионы.xlsx"
+
+class Districts(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    id_district: int = Field()
+    district: str = Field()
+    table_name: ClassVar[str] = "Округа.xlsx"
+
+class Ministry(BaseModel):
+    UniqueID: Optional[int] = Field(default=None, primary_key=True, nullable=None)
+    id_ministry: int = Field()
+    ministry: str = Field()
+    table_name: ClassVar[str] = "Министерства.xlsx"
