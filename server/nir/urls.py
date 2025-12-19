92c157986027088e4994404783cbdb81dd887a05
Nero <doglermr9@gmail.com>
2025-12-19 21:12:32 +0300

    fix: urls

--- server/nir/urls.py
+++ server/nir/urls.py
@@ -23,8 +23,6 @@ from django.http import HttpResponse
 def health_check(request):
     return HttpResponse("OK")
 
-urlpatterns += [path('health/', health_check)]
-
 urlpatterns = [
     path("", lambda request: redirect('vuz/')),
     path("vuz/", include("vuz.urls")),
